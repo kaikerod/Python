b6edb1dc0e4b8513afc322fc2fc5b7f59f0f8a8e
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2025-12-04 19:49:47 -0300

    Add def

--- ex/ex-003.py
+++ ex/ex-003.py
@@ -1,11 +1,11 @@
-name = 'Kaike'
-password = ('kaike1234')
+name_set = 'Kaike'
+password_set = ('kaike1234')
 
 user_name = input('Digite seu usuário para efetuar o login: ')
 user_password = input('Digite sua senha: ')
 
 def login():
-    if user_name == name and user_password == password:
+    if user_name == name_set and user_password == password_set:
         print('Seja bem-vindo!')
     else:
         print('Credenciais inválidas. Tente novamente.')
