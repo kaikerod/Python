03d0b53af25bdb1bfd5ad29f24bdffc68bc52270
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2025-12-03 21:07:40 -0300

    praticando

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,1 +1,24 @@
-print('Hello World')
+import os
+
+print('Sabor express \n')
+
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurantes')
+print('3. Ativar Restaurante')
+print('4. Sair \n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def cadastro():
+    dados_cadastro = []
+    dados_cadastro = input('Preencha com o nome e endereço: \n')
+    
+
+def finalizar_app():
+    os.system('cls')
+    print('Saindo...')
+
+if opcao_escolhida == 1:
+    cadastro()
+else:
+    finalizar_app()
