29a2e3ce46f322d4d39e2d9627f0d4fabbef7973
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2025-12-17 06:56:15 -0300

    exercicios

--- ex/ex-014.py
+++ ex/ex-014.py
@@ -1,10 +1,20 @@
-cardapio = {
-    'X-Burguer': {'preco': 18.00, 'categoria': 'lanche', 'disponivel': True},
-    'Pizza': {'preco': 45.00, 'categoria': 'principal', 'disponivel': True},
-    'Refrigerante': {'preco': 6.00, 'categoria': 'bebida', 'disponivel': True},
+playlist = {
+    'Bohemian Rhapsody': {'artista': 'Queen', 'duracao': 354, 'ano': 1975, 'plays': 1250},
+    'Imagine': {'artista': 'John Lennon', 'duracao': 183, 'ano': 1971, 'plays': 890},
 }
 
-cardapio['Poke'] = {'preco': 25.00, 'categoria': 'principal', 'disponivel': False}
-cardapio['Refrigerante'] = {'preco': 6.00, 'categoria': 'bebida', 'disponivel': False}
+playlist['Hotel California'] = {'artista': 'Eagles', 'duracao': 252, 'ano': 1977, 'plays': 1500}
+playlist['Crippling Alcoholism'] = {'artista': 'Anathema', 'duracao': 245, 'ano': 2000, 'plays': 1000}
 
-print(cardapio)
+total_duracao = 0
+mais_plays = 0
+
+for duracao in playlist.values():
+    total_duracao += duracao['duracao']
+
+print(f'Esta é a duracao total: {total_duracao}')
+
+for mais_tocada in playlist.values():
+    mais_plays += mais_tocada['plays']
+
+print(f'Esta é a musica mais tocada: {mais_plays}')
