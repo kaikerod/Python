6b7861cedaea5060a838f2d6186e6ebe0d7d7bb6
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-01-08 20:57:27 -0300

    Add a new Restaurante class to model restaurant attributes including name, category, and active status

---
+++ oop-sabor-express/modelos/restaurante.py
@@ -0,0 +1,9 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante = Restaurante()
+restaurante.nome = 'Trash and Fresh'
+restaurante.categoria = 'Hamburgueria'
+restaurante.ativo = True
