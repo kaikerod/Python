47a5b308455e0d58002d639aaa44aa2d36105eef
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-01-20 20:00:58 -0300

    Remove Musica and Restaurante classes along with their instances from the project

--- oop-sabor-express/modelos/restaurante.py
+++
@@ -1,9 +0,0 @@
-class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
-
-restaurante = Restaurante()
-restaurante.nome = 'Trash and Fresh'
-restaurante.categoria = 'Hamburgueria'
-restaurante.ativo = True
