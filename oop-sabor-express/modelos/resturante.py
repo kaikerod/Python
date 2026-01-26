eab574629884b301b6a7e6ccaf269fb9e6836c0a
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-01-26 20:38:40 -0300

    Refactor Restaurante class to initialize attributes through constructor, setting default active status to False

--- oop-sabor-express/modelos/resturante.py
+++ oop-sabor-express/modelos/resturante.py
@@ -1,25 +1,10 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Bistro'
-restaurante_praca.categoria = 'Italiana'
-
-if restaurante_praca.ativo == False:
-    print('Restaurante desativado!')
-else:
-    print('Restaurante ativado!')
-
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Pizza Place'
-restaurante_pizza.categoria = 'Fast Food'
-restaurante_pizza.ativo = True
-
-if restaurante_pizza.categoria == 'Fast Food':
-    print('A categoria é Fast Food.')
-else:
-    print('A categoria não é Fast Food.')
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 print(f'Nome: {restaurante_praca.nome}, Categoria: {restaurante_praca.categoria}')
