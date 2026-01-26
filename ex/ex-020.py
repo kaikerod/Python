575ebff0534efee7aaa10c665dda5b9fcc4f036c
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-01-26 19:56:53 -0300

    Add new script ex-020.py

---
+++ ex/ex-020.py
@@ -0,0 +1,14 @@
+class Musicas:
+  nome = ''
+  artista = ''
+  duracao = ''
+
+musica1 = Musicas()
+musica1.nome = 'Always Then'
+musica1.artista = 'KVB'
+musica1.duracao = '3min'
+
+musica2 = Musicas()
+musica2.nome = 'CAMGIRL'
+musica2.artista = 'Crippling Alcoholism'
+musica2.duracao = '7min'
