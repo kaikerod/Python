1784acc4d9f1f15965d6142d0744cd53dc65d369
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-01-26 20:50:24 -0300

    Add string representation to Restaurante class and update print statements for instances

--- oop-sabor-express/modelos/resturante.py
+++ oop-sabor-express/modelos/resturante.py
@@ -4,7 +4,11 @@ class Restaurante:
         self.categoria = categoria
         self.ativo = False
 
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}' 
+
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-print(f'Nome: {restaurante_praca.nome}, Categoria: {restaurante_praca.categoria}')
+print(restaurante_praca)
+print(restaurante_pizza)    
