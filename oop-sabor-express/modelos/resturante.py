5668f6b2344a46753f80a130959de3cf196be588
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-01-27 19:30:33 -0300

    a

--- oop-sabor-express/modelos/resturante.py
+++ oop-sabor-express/modelos/resturante.py
@@ -1,14 +1,20 @@
+from pydoc import describe
+
+
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}' 
+        return f'{self.nome} | {self.categoria}'
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 print(restaurante_praca)
-print(restaurante_pizza)    
+print(restaurante_pizza)
