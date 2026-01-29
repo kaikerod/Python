97097e98dc9066af63d1258cbfc75bd0476f2cdb
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-01-29 20:32:21 -0300

    Add ver_filme method to Cliente class for movie viewing based on subscription plan

--- ex/oop.netflix.py
+++ ex/oop.netflix.py
@@ -14,7 +14,19 @@ class Cliente:
     else:
       print('Plano Inválido!')
 
+    def ver_filme(self, filme, plano_filme):
+      if self.plano == plano_filme:
+        print(f'Ver filme {filme}')
+      elif self.plano == 'Premium':
+        print(f'Ver filme {filme}')
+      elif self.plano == 'Basic' and plano_filme == 'Premium':
+        print('Faça um upgrade do seu plano para acessar este filme.')
+      else:
+        print('Plano Inválido!')
+
 cliente = Cliente('Kaike', 'kaike@gmail.com', 'Basic')
 print(cliente.plano)
+cliente.ver_filme('Zootopia 2', 'Basic')
+
 cliente.mudar_plano('Premium')
 print(cliente.plano)
