a8d78e6a24d060a9414ae666dea951177db8c4c0
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-02 19:56:59 -0300

    a

--- ex/ex-023.py
+++ ex/ex-023.py
@@ -1,5 +1,20 @@
+import math
+
 class Retangulo:
   def __init__(self, largura, altura):
     self.largura = largura
     self.altura = altura
 
+  def calcular_perimetro(self):
+    return 2 * (self.largura + self.altura)
+
+  def calcular_area(self):
+    return self.largura * self.altura
+
+  def calcular_diagonal(self):
+    return math.sqrt(self.largura ** 2 + self.altura ** 2)
+
+retangulo1 = Retangulo(10, 20)
+print(retangulo1.calcular_perimetro())
+print(retangulo1.calcular_area())
+print(retangulo1.calcular_diagonal())
