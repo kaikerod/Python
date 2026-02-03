26d5fcff9e861ae5d0863ec88a390261fb26bb3c
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-03 20:08:29 -0300

    Update Restaurante class to set default 'ativo' status to False and add string representation method

--- ex/restaurante.py
+++ ex/restaurante.py
@@ -2,7 +2,10 @@ class Restaurante:
   def __init__(self, nome, categoria, ativo):
     self.nome = nome
     self.categoria = categoria
-    self.ativo = ativo
+    self.ativo = False
 
-restautante1 = Restaurante('Trash', 'Hamburgueria', 'Ativo')
-print(restautante1.categoria)
+  def __str__(self):
+        return f'{self.nome} | {self.categoria} | {self.ativo}'
+
+restautante1 = Restaurante('Trash', 'Hamburgueria', True)
+print(restautante1)
