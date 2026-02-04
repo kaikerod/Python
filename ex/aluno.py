f108bac66e0fb3d9bfa3798d45c09a0bd9d48147
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-04 20:20:21 -0300

    Add Aluno class for managing student attributes and methods for adding grades and calculating average

---
+++ ex/aluno.py
@@ -0,0 +1,13 @@
+class Aluno:
+
+  lista_notas = []
+
+  def __init__(self, nome):
+    self.nome = nome
+
+  def adicionar_nota(self, nota):
+    Aluno.nota.append(self)
+
+
+  def calcular_media(self, nota):
+    pass
