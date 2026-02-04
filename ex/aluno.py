8e15cede1e472e44deba61271000aa8e61751e55
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-04 20:21:42 -0300

    Refactor Aluno class to manage grades as instance attributes instead of a class variable

--- ex/aluno.py
+++ ex/aluno.py
@@ -1,12 +1,10 @@
 class Aluno:
-
-  lista_notas = []
-
   def __init__(self, nome):
     self.nome = nome
+    self.notas = []
 
   def adicionar_nota(self, nota):
-    Aluno.nota.append(self)
+    self.notas.append(nota)
 
 
   def calcular_media(self, nota):
