4fc92ec086a655e554f6337e5c99dbd07345029f
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-09 20:05:35 -0300

    Add methods to Aluno class for calculating average and listing grades, along with example usage

--- ex/aluno.py
+++ ex/aluno.py
@@ -7,5 +7,20 @@ class Aluno:
     self.notas.append(nota)
 
 
-  def calcular_media(self, nota):
-    pass
+  def calcular_media(self):
+    media = sum(self.notas) / len(self.notas)
+    if media <= 7:
+      return 'Você não atingiu a média!'
+    else:
+      return 'Parabéns, você atingiu a média!'
+
+  def listar_notas(self):
+    for nota in self.notas:
+      print(nota)
+
+
+aluno1 = Aluno('Kaike')
+aluno1.adicionar_nota(7)
+aluno1.adicionar_nota(8)
+print(aluno1.calcular_media())
+print(aluno1.listar_notas())
