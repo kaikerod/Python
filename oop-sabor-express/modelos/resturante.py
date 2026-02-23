aabaf4f97ca5ccedf0da68e94987a6bea79c090d
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-23 19:32:40 -0300

    Refactor Restaurante class to use class methods for listing restaurants and managing 'ativo' status, including a method to toggle the state.

--- oop-sabor-express/modelos/resturante.py
+++ oop-sabor-express/modelos/resturante.py
@@ -10,16 +10,21 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self.categoria}'
 
-    def listar_restaurantes():
-        print(f'{'Nome do restaurante'.ljust(10)} | {'Categoria'.ljust(10)} | {'Status'}\n')
-        for restaurante in Restaurante.restaurantes:
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(10)} | {'Categoria'.ljust(10)} | {'Status'}')
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(10)} | {restaurante.categoria.ljust(10)} | {restaurante.ativo}')
 
     @property
-    def ativo(self):
-        return '✅' if self._ativo else '❌'
+    def ativo(cls):
+        return '✅' if cls._ativo else '❌'
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
-    restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
+restaurante_praca.alternar_estado()
 Restaurante.listar_restaurantes()
