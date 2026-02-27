049c0af31dea3b931bd8a24f1e0cc3c74a37ca07
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-27 19:57:06 -0300

    mudando o jeito que as coisas aparecem no terminal

--- ex-2/tarefaoop.py
+++ ex-2/tarefaoop.py
@@ -1,10 +1,11 @@
 class Tarefa:
-  def __init__(self, tarefa):
+  def __init__(self, tarefa, descricao):
     self.tarefa = tarefa
+    self.descricao = descricao
     self._concluida = False
 
   def __str__(self):
-    return f'{self.tarefa:<25} | {self.concluida}'
+    return f'{self.tarefa:<20} | {self.descricao:<35} | {self.concluida}'
 
   @property
   def concluida(self):
@@ -21,20 +22,28 @@ class GerenciadorTarefas:
     self.lista_tarefas.append(tarefa)
 
   def listar_tarefas(self):
+    print(f"{'Tarefa':<20} | {'Descrição':<35} | {'Status'}")
+    print("-" * 75)
     for tarefa in self.lista_tarefas:
       print(tarefa)
 
   def listar_tarefas_pendentes(self):
+    print(f"{'Tarefa':<20} | {'Descrição':<35} | {'Status'}")
+    print("-" * 75)
     for tarefa in self.lista_tarefas:
       if not tarefa._concluida:
         print(tarefa)
 
+  def remover_tarefa(self, tarefa_para_remover):
+    self.lista_tarefas.remove(tarefa_para_remover)
+    print('Tarefa apagada do sistema!')
+
 gerenciador = GerenciadorTarefas()
 
-t1 = Tarefa('Estudar Python')
-t2 = Tarefa('Fazer exercícios de POO')
-t3 = Tarefa('Descansar a mente')
-t4 = Tarefa('Limpar o carro')
+t1 = Tarefa('Estudar Python', 'Estudar Python por 45min')
+t2 = Tarefa('Fazer exercícios de POO', 'Estudar POO')
+t3 = Tarefa('Descansar a mente', 'Descansar 15min')
+t4 = Tarefa('Limpar o carro', 'Lavar o carro sujo')
 
 # Adicionando ao gerenciador
 gerenciador.adicionar_tarefa(t1)
