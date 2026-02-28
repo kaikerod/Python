a6fd498fc35d77b7aa4a42d37f4575e549cbc8f3
Kaike Rodrigues <kaikerodriguez99@gmail.com>
2026-02-28 11:30:00 -0300

    Implement sales analysis system with `Venda` and `AnaliseVendas` classes.

--- ex-2/vendas.py
+++ ex-2/vendas.py
@@ -16,9 +16,28 @@ class AnaliseVendas:
   def adicionar_venda(self, venda):
     self.lista_vendas.append(venda)
 
+  def vendas_acima_de(self, valor_minimo):
+    vendas_filtradas = [venda for venda in self.lista_vendas if venda.valor > valor_minimo]
+    return vendas_filtradas
+
 meu_sistema = AnaliseVendas()
 v1 = Venda('Notebook', 2500.00, date.today())
+v2 = Venda('Smartphone', 1500.00, date(2025, 12, 25))
+v3 = Venda('Tablet', 1000.00, date(2025, 1, 1))
 meu_sistema.adicionar_venda(v1)
+meu_sistema.adicionar_venda(v2)
+meu_sistema.adicionar_venda(v3)
 
 for i in meu_sistema.lista_vendas:
   print(i)
+
+print("\n")
+
+vendas_2k = meu_sistema.vendas_acima_de(2000)
+if vendas_2k:
+  print("Acima de 2000:")
+  print("-" * 60)
+  for i in vendas_2k:
+    print(i)
+else:
+  print("Nenhuma venda acima de R$ 2000.00")
