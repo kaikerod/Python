360144414fd9edaff403bb2a2daab01b6b0183d2
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-24 20:38:25 -0300

    Append na lista
    Append na lista

--- manipulando-dados/input_data.py
+++ manipulando-dados/input_data.py
@@ -1,6 +1,8 @@
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
+# append em arquivo com a função open (adiciona na ultima linha)
+# se estiver usando 'w' ele vai sobrescrever o arquivo
 with open("manipulando-dados/arquivo.txt", "a") as f:
   f.write(f"Nome: {nome}\n")
   f.write(f"Idade: {idade}\n")
