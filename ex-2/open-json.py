8661d7ae5782ccab58048105694afd7b01d9b92c
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-24 20:03:53 -0300

    Manipulando JSON
    Manipulando JSON

---
+++ ex-2/open-json.py
@@ -0,0 +1,13 @@
+import json
+
+# dados em formato de dicionário
+dados = {'nome':'Ana', 'idade':20, 'enderecos':['a', 'b']}
+
+# Escrever arquivo json
+with open("ex-2/arquivo.json", "w") as f:
+  json.dump(dados, f)
+
+# Ler arquivo json
+with open("ex-2/arquivo.json", "r") as f:
+  dados = json.load(f)
+  print(dados)
