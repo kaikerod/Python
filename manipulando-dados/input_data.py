357c1524f967e91b9c0b6eb7f3ee44fc12ddd961
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-24 20:34:20 -0300

    Update input_data.py

--- manipulando-dados/input_data.py
+++ manipulando-dados/input_data.py
@@ -1,6 +1,6 @@
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
-with open("manipulando-dados/arquivo.txt", "w") as f:
+with open("manipulando-dados/arquivo.txt", "a") as f:
   f.write(f"Nome: {nome}\n")
   f.write(f"Idade: {idade}\n")
