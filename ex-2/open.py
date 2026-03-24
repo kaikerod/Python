99e01d187f92964472a896590950910b487e2170
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-24 19:34:15 -0300

    Comentários
    Comentários

--- ex-2/open.py
+++ ex-2/open.py
@@ -1,8 +1,14 @@
 # escrever arquivo com a função open
+# open(caminho_do_arquivo, modo)
+# modos: 'w' (write), 'r' (read)
 with open("ex-2/arquivo.txt", "w") as f:
-  f.write("Hello World")
+  f.write("Hello World\n")
 
 # ler arquivo com a função open
 with open("ex-2/arquivo.txt", "r") as f:
   conteudo = f.read()
   print(conteudo)
+
+# append em arquivo com a função open (adiciona na ultima linha)
+with open("ex-2/arquivo.txt", "a") as f:
+  f.write("Ola mundo!")
