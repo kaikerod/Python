f654c744acc49bacaee1660721ada6fe0f3b5710
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-24 20:13:08 -0300

    Mudando a pasta
    Mudando a pasta

--- manipulando-dados/open-csv.py
+++ manipulando-dados/open-csv.py
@@ -2,13 +2,13 @@ import csv
 
 # Escrever arquivo csv
 # newline='' evita que o arquivo fique com linhas em branco
-with open("ex-2/arquivo.csv", "w", newline='') as f:
+with open("manipulando-dados/arquivo.csv", "w", newline='') as f:
   escritor = csv.writer(f)
   escritor.writerow(['nome', 'idade']) # escreve uma linha
   escritor.writerow(['Kaike', '20'])
 
 # Ler arquivo csv
-with open("ex-2/arquivo.csv", "r", newline='') as f:
+with open("manipulando-dados/arquivo.csv", "r", newline='') as f:
   leitor = csv.reader(f)
   for linha in leitor:
     print(linha)
--- manipulando-dados/open-json.py
+++ manipulando-dados/open-json.py
@@ -4,10 +4,10 @@ import json
 dados = {'nome':'Ana', 'idade':20, 'enderecos':['a', 'b']}
 
 # Escrever arquivo json
-with open("ex-2/arquivo.json", "w") as f:
+with open("manipulando-dados/arquivo.json", "w") as f:
   json.dump(dados, f)
 
 # Ler arquivo json
-with open("ex-2/arquivo.json", "r") as f:
+with open("manipulando-dados/arquivo.json", "r") as f:
   dados_lidos = json.load(f)
   print(dados_lidos)
