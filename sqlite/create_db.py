2aecb15d3ee4f3c75d04459f16e34a166f023b25
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-26 11:16:34 -0300

    Estudando SQLite, Criando DB

---
+++ sqlite/create_db.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+#cria conexão com o banco de dados/cria o arquivo escola.db se não existir
+conn = sqlite3.connect("escola.db")
+
+#cria cursor para executar comandos sql
+cursor = conn.cursor()
+
+#cria tabela alunos
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS estudantes (
+    id INTEGER PRIMARY KEY,
+    nome TEXT,
+    idade INTEGER
+)
+""")
+
+#cria tabela disciplinas
+#FOREIGN KEY (estudante_id) REFERENCES estudantes(id) -> cria relação entre as tabelas
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS disciplinas (
+    id INTEGER PRIMARY KEY,
+    nome TEXT,
+    estudante_id INTEGER,
+    FOREIGN KEY (estudante_id) REFERENCES estudantes(id)
+)
+""")
