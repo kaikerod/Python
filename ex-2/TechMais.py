7917c70f5ea3448a1037b8da1b8102c3757d2f7e
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-26 18:11:05 -0300

    Create TechMais.py

---
+++ ex-2/TechMais.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+def conectar_db():
+  conn = sqlite3.connect("loja.db")
+  return conn
+
+def criar_tabela_produtos():
+  conn = conectar_db()
+  cursor = conn.cursor()
+  cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS produtos (
+      id INTEGER PRIMARY KEY,
+      nome TEXT,
+      preco REAL
+    )
+    """
+  )
+  conn.commit()
+  conn.close()
+
+def inserir_produto():
+  conn = conectar_db()
+  cursor = conn.cursor()
+  cursor.execute(
+    """
+    INSERT INTO produtos (nome, preco) 
+    VALUES (?, ?)
+    """
+  )
+  conn.commit()
+  conn.close()
+
+def listar_produtos_cadastrados():
+  conn = conectar_db()
+  cursor = conn.cursor()
+  cursor.execute(
+    """
+    SELECT * FROM produtos
+    """
+  )
+  produtos = cursor.fetchall()
+  conn.close()
+  return produtos
