af87244285f10bfe2b24a3df1b1c171c6cd7e54e
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-27 17:12:56 -0300

    Criando o DB e as tabelas
    Criando o DB e as tabelas

---
+++ modelagem-persistencia/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import relationship
+from database import Base
+
+class Estudante(Base):
+    __tablename__ = "estudantes"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    email = Column(String)
+    perfil = relationship("Perfil", back_populates="estudante", uselist=False, cascade="all, delete-orphan") # Relacionamento entre estudante e perfil
+# Uselist=False indica que o relacionamento é de um para um
+# Back_populates indica que o relacionamento é bidirecional entre estudante e perfil
+# Cascade="all, delete-orphan" indica que o relacionamento é de um para um, se o estudante for deletado, o perfil também será deletado
+
+class Perfil(Base):
+    __tablename__ = "perfis"
+    id = Column(Integer, primary_key=True, index=True)
+    idade = Column(Integer)
+    endereco = Column(String)
+    estudante_id = Column(Integer, ForeignKey("estudantes.id"), unique=True)
+    estudante = relationship("Estudante", back_populates="perfil")
+# Unique=True indica que o perfil só pode ter um estudante
+# Back_populates indica que o relacionamento é bidirecional entre estudante e perfil
