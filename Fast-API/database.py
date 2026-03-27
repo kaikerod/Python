e28a8e44ff7096377605e59c70f0e737144cfa8f
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-27 10:26:32 -0300

    Update database.py

--- Fast-API/database.py
+++ Fast-API/database.py
@@ -1,1 +1,18 @@
 # Arquivo para criar a conexão com o banco de dados
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import sessionmaker
+
+# Conexão com o banco de dados PostgreSQL | define usuário, senha, host e nome do banco
+#                             usuário/  senha/  host/     nome do banco
+DATABASE_URL = "postgresql://postgres:123456@localhost/escola"
+
+# Motor do banco de dados, comunica com o FastAPI
+engine = create_engine(DATABASE_URL)
+
+# Cria a sessão de conexão com o banco de dados
+SessionLocal = sessionmaker(bind=engine)
+
+# Cria a base para os modelos
+Base = declarative_base()
