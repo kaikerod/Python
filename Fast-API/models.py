574044e5ff487139edc6a9450885885285666e7a
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-27 10:40:45 -0300

    Criando tabelas do DB
    Criando tabelas do DB

--- Fast-API/models.py
+++ Fast-API/models.py
@@ -1,1 +1,16 @@
 # Define as entidades da aplicação, ex: produtos, clientes, etc.
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class Estudante(Base):
+  __tablename__ = "estudantes"
+  id = Column(Integer, primary_key=True, index=True) # index=True cria um índice que automaticamente incrementa o valor
+  nome = Column(String(100), nullable=False) # nullable=False significa que o campo não pode ser nulo e String(100) define o tamanho máximo do campo
+  idade = Column(Integer, nullable=False)
+
+class Matricula(Base):
+  __tablename__ = "matriculas"
+  id = Column(Integer, primary_key=True, index=True)
+  estudante_id = Column(Integer, ForeignKey("estudantes.id"))
+  nome_disciplina = Column(String(100), nullable=False)
