8403c8c95fd9b34788d14a09074c9058dc592f2c
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-03-27 11:11:10 -0300

    Update schemas.py

--- Fast-API/schemas.py
+++ Fast-API/schemas.py
@@ -1,1 +1,31 @@
 # Valida os dados que chegam na API
+
+from pydantic import BaseModel
+
+# Garante que os dados que chegam na API estão corretos, ex: nome é string e idade é int
+class EstudanteBase(BaseModel):
+  nome: str
+  idade: int
+
+class EstudanteCreate(EstudanteBase):
+  pass
+
+# Herda de EstudanteBase e adiciona o campo id
+class Estudante(EstudanteBase):
+  id: int
+
+  class Config:
+    from_attributes = True # Informa para o Pydantic que ele deve ler os atributos do modelo SQLAlchemy
+
+class MatriculaBase(BaseModel):
+  estudante_id: int
+  nome_disciplina: str
+
+class MatriculaCreate(MatriculaBase):
+  pass
+
+class MatriculaResponse(MatriculaBase):
+  id: int
+
+  class Config:
+    from_attributes = True
