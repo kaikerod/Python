59b9f4ccfa15a39b1a3e0abe9bf31528bd6815b6
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-04-04 11:34:47 -0300

    Update database.py

--- ex-4/biblioteca/database.py
+++ ex-4/biblioteca/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://postgres:123456@localhost:5432/db_biblioteca"
+DATABASE_URL = "postgresql+psycopg2://postgres:123456@localhost:5432/db_biblioteca"
 
 engine = create_engine(DATABASE_URL, connect_args={"client_encoding": "utf8"})
 SessionLocal = sessionmaker(bind=engine)
