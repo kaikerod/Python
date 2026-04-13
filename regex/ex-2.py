59a41f5279d15c1b4585df4d163ae64751cdfca1
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-04-13 20:06:09 -0300

    Update ex-2.py

--- regex/ex-2.py
+++ regex/ex-2.py
@@ -3,4 +3,4 @@ import re
 cliente = input("Digite o nome do cliente: ")
 cidade = input("Digite o nome da cidade do cliente: ")
 
-print('Olá, {}! Como vai você?'.format(cliente))
+print('Olá, {}! Bem-vindo(a) à {}!'.format(cliente, cidade))
