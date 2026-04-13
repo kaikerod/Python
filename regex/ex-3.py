02fe67f81929c57e751d43ce8da7d9e82d513b5b
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-04-13 20:10:23 -0300

    Create ex-3.py

---
+++ regex/ex-3.py
@@ -0,0 +1,9 @@
+import re
+
+palavra = input('Digite a palavra chave: ')
+
+primeiras_letras = palavra[:3]
+ultimas_letras = palavra[-3:]
+
+print(f'As primeiras letras são: {primeiras_letras}')
+print(f'As ultimas letras são: {ultimas_letras}')
