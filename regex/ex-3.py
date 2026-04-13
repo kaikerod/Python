b2c22c1d0a4d5f758ef1280eb741787c28796290
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-04-13 20:48:48 -0300

    Update ex-3.py

--- regex/ex-3.py
+++ regex/ex-3.py
@@ -1,9 +1,7 @@
-import re
-
 palavra = input('Digite a palavra chave: ')
 
-primeiras_letras = str(palavra[:3])
-ultimas_letras = str(palavra[-3:])
+primeiras_letras = palavra[:3]
+ultimas_letras = palavra[-3:]
 
 print(f'As primeiras letras são: {primeiras_letras}')
 print(f'As ultimas letras são: {ultimas_letras}')
