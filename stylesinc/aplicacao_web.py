7413bdd29480fc30743fcc2f716d252193b0252c
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-04-14 19:51:04 -0300

    Update aplicacao_web.py

--- stylesinc/aplicacao_web.py
+++ stylesinc/aplicacao_web.py
@@ -1,3 +1,7 @@
 from wsgiref.simple_server import make_server
 
+def aplicacao_web(environ, start_response): # 
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b'Hello World']
+
 make_server('', 5000, 'aplicacao_web') # host (local), porta, aplicação
