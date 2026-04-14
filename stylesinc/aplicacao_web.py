3aeebeb8a9fedaf201dc7d751ed516f8005b1e96
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-04-14 19:56:53 -0300

    Update aplicacao_web.py

--- stylesinc/aplicacao_web.py
+++ stylesinc/aplicacao_web.py
@@ -1,7 +1,19 @@
 from wsgiref.simple_server import make_server
 
-def aplicacao_web(environ, start_response): # 
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b'Hello World']
+def aplicacao_web(environ, start_response): # environ: dicionário com informações da requisição, start_response: função que envia a resposta
+    start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
+    html = b'''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+  
+    </body>
+    </html>
+    '''
 
 make_server('', 5000, 'aplicacao_web') # host (local), porta, aplicação
