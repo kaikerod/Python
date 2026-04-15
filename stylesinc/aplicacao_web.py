a929550d872250089b26fb1f53743ec4ae7f9023
Kaike Rodrigues <167778776+kaikerod@users.noreply.github.com>
2026-04-15 20:12:17 -0300

    Update aplicacao_web.py

--- stylesinc/aplicacao_web.py
+++ stylesinc/aplicacao_web.py
@@ -1,26 +1,18 @@
 from wsgiref.simple_server import make_server
 
 
+import os
+
 def aplicacao_web(environ, start_response):  # environ: dicionário com informações da requisição; start_response: função que envia a resposta
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
-    with open('index.html', 'r', encoding='utf-8') as file:
+    
+    # Get the directory where the script is located
+    base_path = os.path.dirname(__file__)
+    file_path = os.path.join(base_path, 'index.html')
+    
+    with open(file_path, 'r', encoding='utf-8') as file:
         html = file.read()
     return [html.encode('utf-8')]
-    
-    html = '''
-    <!DOCTYPE html>
-    <html lang="pt-BR">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>Olá mundo</h1>
-    </body>
-    </html>
-    '''.encode('utf-8')
-    return [html]
 
 
 if __name__ == '__main__':
